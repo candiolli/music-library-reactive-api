ea05141dc45a19c4da5563172a4786e77238ff22
silascandiolli <silascandiolli@cwi.com.br>
2019-03-20 14:41:25 -0300

    first commit

---
+++ src/main/java/com/example/musiclibraryreactiveapi/MusicLibraryReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.musiclibraryreactiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLibraryReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLibraryReactiveApiApplication.class, args);
+	}
+
+}
