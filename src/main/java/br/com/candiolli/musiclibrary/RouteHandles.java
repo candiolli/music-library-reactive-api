df809942657309d136c6760d6042ed0b7ae2ee7a
Silas Candiolli <silasca@gmail.com>
2019-04-08 15:35:50 -0300

    revert coments

--- src/main/java/br/com/candiolli/musiclibrary/RouteHandles.java
+++ src/main/java/br/com/candiolli/musiclibrary/RouteHandles.java
@@ -55,43 +55,11 @@ public class RouteHandles {
 
     public Mono<ServerResponse> libraryCreate(ServerRequest req) {
         Mono<Library> library = req.bodyToMono(Library.class);
-        //doOnError(thr -> thr.printStackTrace())
 
         Mono<ServerResponse> serverResponseMono = library
-//                .map(lib -> {
-//                    lib.getMusicsIds()
-//                            .()
-//                            .flatMap(m -> {
-//                                musicService.byId(m).doOnError(throwable -> throwable.printStackTrace());
-//                            })
-//                            .collect(lib);
-//                })
-                .map(lib2 -> this::validate)
                 .flatMap(lib -> libraryService.save(lib))
                 .flatMap(p -> ServerResponse.created(URI.create("/library/" + p.getId())).build());
         return serverResponseMono;
     }
 
-    private Mono validate(Library lib) {
-
-    }
-//    public ServerResponse.BodyBuilder musicCreate(ServerRequest serverRequest) {
-//        Mono<Music> musicMono = serverRequest.bodyToMono(Music.class);
-//        musicMono.flatMap(music -> musicService.save(music));
-//        return ServerResponse.ok();
-//                .flatMap(music,  Music.class)
-//                .doOnError(throwable -> new IllegalStateException("oh boy... not againnn =(("));
-
-//        return ServerResponse.ok()
-//                .body(musicService.save(music), Music.class)
-//                .doOnError(throwable -> new IllegalStateException("oh boy... not againnn =(("));
-//    }
-
-//    public Mono<ServerResponse> events(ServerRequest serverRequest) {
-//        String carId = serverRequest.pathVariable("carId");
-//        return ServerResponse.ok()
-//                .contentType(MediaType.TEXT_EVENT_STREAM)
-//                .body(musicService.streams(carId), CarEvents.class)
-//                .doOnError(throwable -> new IllegalStateException("I give up!! "));
-//    }
 }
